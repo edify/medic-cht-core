114773f75d46f341ac4298fbb7ca70606db45d64
Sigifredo-Chacon <sigifredo.chacon@edify.cr>
2026-03-30 11:53:48 -0600

    fixing lint errors

--- admin-tool/tests/karma/ts/services/settings.service.spec.ts
+++ admin-tool/tests/karma/ts/services/settings.service.spec.ts
@@ -26,7 +26,9 @@ describe('SettingsService', () => {
       subscribe: sinon.stub().callsFake((options) => {
         changesCallback = options.filter
           ? (change) => {
-              if (options.filter(change)) options.callback(change);
+              if (options.filter(change)) {
+                options.callback(change);
+              }
             }
           : options.callback;
         return { unsubscribe: sinon.stub() };
